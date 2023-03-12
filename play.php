bdbac688b9f3c8869895dfc239c520af39ab48d0
Dnyanesh Painjane <81969659+dnyaneshpainjane@users.noreply.github.com>
2023-03-12 20:07:49 +0530

    Update play.php

--- play.php
+++ play.php
@@ -10,8 +10,8 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <!--===================================ICONS================================-->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <title>Dnyanesh | MXLIVE</title>
-    <link rel="shortcut icon" type="image/x-icon" href="https://m.media-amazon.com/images/I/515+F2vlqtL.png">
+    <title>TataPlay Online</title>
+    <link rel="shortcut icon" href="favicon.ico"/>
 <style>
 
       html,
